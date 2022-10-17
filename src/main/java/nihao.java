407631ed29ae06fec0d2e1620286d92fa32e3b2f
LuoAnjun2000 <115981484+LuoAnjun2000@users.noreply.github.com>
2022-10-17 19:06:34 +0800

    Update nihao.java

--- src/main/java/nihao.java
+++ src/main/java/nihao.java
@@ -2,5 +2,6 @@ public class nihao {
     public static void main(String[] args) {
         System.out.println("nihao");
         System.out.println("HelloWord");
+        System.out.println("Helloword");
     }
 }
